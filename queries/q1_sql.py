b4dcc14c2cb696e73505a1241d7f16db7b3d58c8
vouts96 <bivos96@gmail.com>
2023-02-05 15:45:43 +0200

    q1,q2 update

--- queries/q1_sql.py
+++ queries/q1_sql.py
@@ -8,23 +8,33 @@ from pyspark.sql.types import *
 
 spark = SparkSession.builder.appName('Q1_SQL').getOrCreate()
 
+#Create Dataframe for tripdata
 df_trips = spark.read.parquet('hdfs://master:9000/project_data/*.parquet')
 
+#Create Dataframe for zones
+df_zones = spark.read.csv('hdfs://master:9000/project_data/taxi+_zone_lookup.csv', header=True)
+
+#Join Zones with Tripdata
+df_trips = df_trips.join(df_zones, df_trips['DOLocationID'] ==  df_zones['LocationID'],"inner")
+
+#Change pickup date timestamp
 df_trips = df_trips.withColumn('pickup_date', to_date(df_trips.tpep_pickup_datetime,'YYYY-MM-DD'))
+
+#Create Sql View
 df_trips.createOrReplaceTempView("tripdata")
 
 start_time = time.time()
 
-sqlDF1 = spark.sql("SELECT\
-    DOLocationID,\
-    tip_amount,\
-    pickup_date\
+sqlDF1 = spark.sql("SELECT *\
     FROM tripdata\
     WHERE tip_amount =\
         (SELECT MAX(tip_amount) FROM tripdata\
-        WHERE pickup_date BETWEEN '2022-03-01' AND '2022-03-31' AND DOLocationID = 12)\
-        AND DOLocationID = 12").show()
+        WHERE pickup_date BETWEEN '2022-03-01' AND '2022-03-31' AND Zone = 'Battery Park')\
+        AND Zone = 'Battery Park'")
+
+sqlDF1.collect()
+sqlDF1.show()
 
 time = time.time() - start_time
 
-print("Execution time:", time)
+print('Execution time:', time)
--- queries/q2_sql.py
+++ queries/q2_sql.py
@@ -8,20 +8,37 @@ from pyspark.sql.types import *
 
 spark = SparkSession.builder.appName('Q2_SQL').getOrCreate()
 
+#Create Dataframe for tripdata
 df_trips = spark.read.parquet('hdfs://master:9000/project_data/*.parquet')
+
+#Create Dataframe for zones
+df_zones = spark.read.csv('hdfs://master:9000/project_data/taxi+_zone_lookup.csv', header=True)
+
+#Join Zones with Tripdata
+df_trips = df_trips.join(df_zones, df_trips['DOLocationID'] ==  df_zones['LocationID'],"inner")
+
+#Change pickup date timestamp
+df_trips = df_trips.withColumn('pickup_date', to_date(df_trips.tpep_pickup_datetime,'YYYY-MM-DD'))
+
+#Create Sql View
+df_trips.createOrReplaceTempView("tripdata")
 
 start_time = time.time()
 
-df_trips = df_trips.withColumn('pickup_date', to_date(df_trips.tpep_pickup_datetime,'YYYY-MM-DD'))
-df_trips.createOrReplaceTempView("tripdata")
-
 #Q2 
-sql2 = spark.sql("SELECT MAX(tolls_amount),\
-    MONTH(pickup_date)\
+sql2 = spark.sql("SELECT t1.*\
+    FROM tripdata t1\
+    JOIN\
+    (SELECT MAX(tolls_amount) AS tolls,\
+    MONTH(pickup_date) AS pickup\
     FROM tripdata\
     WHERE MONTH(pickup_date) < 7\
     GROUP BY MONTH(pickup_date)\
-    ORDER BY MAX(tolls_amount) DESC").show()
+    ORDER BY MAX(tolls_amount) DESC) t2\
+    ON t1.tolls_amount = t2.tolls")
+
+sql2.collect()
+sql2.show()
 
 time = time.time() - start_time
 
